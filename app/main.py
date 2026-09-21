ca7f6b6fcf2d00adc4dbec42ad043223d9a38316
Ray Samalo <raymond.samalo@gmail.com>
2026-09-21 22:39:18 +0800

    refactor layout

--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 from nicegui import app as nicegui_app
 from nicegui import ui
 from contextlib import asynccontextmanager
-from app import backend, frontend
+from app import backend
 from app.dependencies import repo
+from app.ui import main_ui
 
 
 @asynccontextmanager
@@ -17,5 +18,5 @@ async def lifespan(app: FastAPI):
 fastapi_app = FastAPI(lifespan=lifespan)
 # we use router here to allow modular backends for our api
 
-ui.run_with(fastapi_app, mount_path="/ui", root=frontend.main_page)
+ui.run_with(fastapi_app, mount_path="/ui", root=main_ui.main_page)
 fastapi_app.include_router(backend.router)
