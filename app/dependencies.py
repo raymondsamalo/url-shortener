d0e49f0a3ba72541fa5a33fdd3c6d7f6b3d7cbbd
Ray Samalo <raymond.samalo@gmail.com>
2026-09-22 13:20:09 +0800

    final candidate

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,11 @@
+import os
+import sqlite3
 from app.repository import InMemoryURLMapRepository, DatabaseURlMapRepository
-
-# declare our repository -> configurable using config file
-repo = DatabaseURlMapRepository("/tmp/database.db")
+import pathlib
+# Finds the absolute directory of the current script
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = pathlib.Path(BASE_DIR).parent.joinpath("database.db").resolve().as_posix()
+print(db_path)
+# declare our repository -> configurable using config file in future
+repo = DatabaseURlMapRepository(db_path)
 # repo = DatabaseURlMapRepository(db_path=None)
